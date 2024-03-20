c2adb5a4c8b96102e687665f6b373764a79ecd4e
fdhhhdjd <nguyentientai10@gmail.com>
2024-03-20 15:17:05 +0700

    first commit

--- src/app/v1/routes/index.js
+++ src/app/v1/routes/index.js
@@ -2,9 +2,22 @@
 
 //* LIB
 const express = require("express");
+const {
+  ReasonPhrases,
+  StatusCodes,
+} = require("../../../commons/utils/httpStatusCode");
 
 const router = express.Router();
 
-router.use("/redis", require("./redis"));
+router.use("/login", require("./auth"));
+
+router.get("/", async (_, res, __) => {
+  const healthCheck = {
+    uptime: process.uptime(),
+    message: ReasonPhrases.OK,
+    timestamp: Date.now(),
+  };
+  return res.status(StatusCodes.OK).json(healthCheck);
+});
 
 module.exports = router;
